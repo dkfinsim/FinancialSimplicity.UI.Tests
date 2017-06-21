5edef90e7266e409e9bfe468ba204e3ee6ffe93b
Michael Henstock <michael.henstock@financialsimplicity.com>
2017-06-21 14:48:07 +1000

    msbuild script to clean up azure endpoint, make sure clean up script is authenticated

--- AzureCleanUp.ps1
+++ AzureCleanUp.ps1
@@ -1,3 +1,11 @@
+Import-Module AzureRM -Version 4.0.0
+Import-Module AzureRM.profile -Version 3.0.1
+
+$authCtx = Import-AzureRmContext -Path $env:UserProfile\AzureAuth.json
+############ work around for auth bug in azure powershell scripts
+$authCtx.Context.TokenCache.Deserialize($authCtx.Context.TokenCache.CacheData)
+############
+
 $resourceGroup = "ui_testing_resources"
 
 Remove-AzureRmResourceGroup -Name $resourceGroup -Force
