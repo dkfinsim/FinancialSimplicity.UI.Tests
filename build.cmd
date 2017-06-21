c94d75ff7eaec9dc353f3e8b29e0caed01ec3702
Michael Henstock <michael.henstock@financialsimplicity.com>
2017-06-21 10:03:28 +1000

    change to build against net452

--- build.cmd
+++ build.cmd
@@ -26,13 +26,13 @@ if "%1"=="azure" GOTO :azureconfig
 GOTO :localconfig
 
 :localconfig
-copy TestFSConfig.config test\FinancialSimplicity.UI.Tests\bin\%config%\net462
+copy TestFSConfig.config test\FinancialSimplicity.UI.Tests\bin\%config%\net452
 set domain_url=https://localhost:44399
-test\FinancialSimplicity.UI.Tests\bin\%config%\net462\FinancialSimplicity.UI.Tests.exe %1
+test\FinancialSimplicity.UI.Tests\bin\%config%\net452\FinancialSimplicity.UI.Tests.exe %1
 GOTO :EOF
 
 :azureconfig
-copy AzureFSConfig.config test\FinancialSimplicity.UI.Tests\bin\%config%\net462\TestFSConfig.config
+copy AzureFSConfig.config test\FinancialSimplicity.UI.Tests\bin\%config%\net452\TestFSConfig.config
 set domain_url=http://uitestvm.australiaeast.cloudapp.azure.com
-test\FinancialSimplicity.UI.Tests\bin\%config%\net462\FinancialSimplicity.UI.Tests.exe %2
+test\FinancialSimplicity.UI.Tests\bin\%config%\net452\FinancialSimplicity.UI.Tests.exe %2
 GOTO :EOF
